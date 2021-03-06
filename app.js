ca99c9802345de9f3213a87bed7ce6a9ab21c3d4
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-03-06 16:40:36 +0530

    Updated with MongoDB

--- app.js
+++ app.js
@@ -4,20 +4,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-const posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = new mongoose.Schema ({
+  title: String,
+  content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 app.get('/', function(req, res) {
-  res.render('home', {homeContent: homeStartingContent, newContent: posts});
+  Post.find({}, function(err, posts) {
+    res.render('home', {homeContent: homeStartingContent, posts: posts});
+  });
 });
 
 app.get('/about', function(req, res) {
@@ -33,22 +43,23 @@ app.get('/compose', function(req, res) {
 });
 
 app.post('/compose', function(req, res) {
-  const post = {
-    Title: req.body.composeTitle,
-    Content: req.body.composeContent
-  };
-  posts.push(post);
-  res.redirect('/');
+
+  const newPost = new Post ({
+    title: req.body.composeTitle,
+    content: req.body.composeContent
+  });
+  newPost.save(function(err) {
+    if(!err) {
+      res.redirect('/');
+    }
+  });
 });
 
-app.get('/posts/:topic', function(req, res) {
-  const requestedTopic = _.kebabCase(req.params.topic);
+app.get('/posts/:postId', function(req, res) {
+  const requestedPostId = _.kebabCase(req.params.postId);
 
-  posts.forEach(function(e) {
-    const storedTitle = _.kebabCase(e.Title);
-    if(storedTitle === requestedTopic) {
-      res.render('post', {storedTitle: e.Title, storedContent: e.Content});
-    }
+  Post.findOne({_id: requestedPostId}, function(err, post) {
+    res.render('post', {storedTitle: post.title, storedContent: post.content});
   });
 });
 
